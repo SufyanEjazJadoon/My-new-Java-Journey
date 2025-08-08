8793837ce5ebc96dbf5243de83f56f6c05c1465e
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-08 22:18:46 +0500

    sum program

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public static void main(String[] args) {
+        System.out.println("The sum of numbers");
+        int num1 = 6;
+        int num2 = 5;
+        int num3 = 7;
+       int sum = num1 + num2 + num3;
+        System.out.println(sum);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
     }
 }
