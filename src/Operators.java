1337a614c4f0c4613d48830512f242f5fb822385
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-10 16:47:33 +0500

    Operators & Precedence and Associativity

---
+++ src/Operators.java
@@ -0,0 +1,30 @@
+public class Operators {
+    public static void main(String[] args) {
+        //arthimatic operators
+        int a = 5;
+        int b = 10;
+        b *= 2;
+        System.out.println(b);
+
+        System.out.println(10>11); // comparison operators
+
+        //bitwise operators
+        System.out.println(10>9 && 10> 7);
+
+//precednece and Associativity
+
+        int a1 = 60/5-34*2;
+
+        System.out.println(a1);
+
+        int x = 20;
+        int y = 5;
+        int z = 2;
+        //int k = x*y/5;
+        int k = y*y - (4*x*y)/(2*x);
+        System.out.println(k);
+
+
+
+    }
+}
