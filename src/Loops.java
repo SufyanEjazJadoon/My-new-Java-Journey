684ecd530b704f0d11c712c09298bda07b777af3
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-13 23:02:36 +0500

    While, do-while,

--- src/Loops.java
+++ src/Loops.java
@@ -2,6 +2,7 @@ public class Loops {
     public static void main(String[] args) {
 
         //While loop
+        System.out.println("While loop ");
         int a = 1;
         while (a <= 10) {
             System.out.println(a);
@@ -15,8 +16,23 @@ public class Loops {
             b++;
 
     }
+// do while loop
+        System.out.println(" ");
+        System.out.println("Do while loop");
+        System.out.println(" ");
+        int b1 = 5;
+        do {
+            System.out.println(b1);
+            b1 ++;
+        }
+        while (b1<= 15);
 
-
+        System.out.println( " New question");
+        int c = 0;
+        do {
+            System.out.println(c);
+            c ++;
+        }while (c <= 21);
 
 }
 }
