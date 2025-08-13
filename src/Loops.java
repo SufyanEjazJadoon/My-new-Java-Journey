db6a8af8200da7944d4f978d5d0b0d245ded95cf
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-13 22:41:14 +0500

    While,

---
+++ src/Loops.java
@@ -0,0 +1,22 @@
+public class Loops {
+    public static void main(String[] args) {
+
+        //While loop
+        int a = 1;
+        while (a <= 10) {
+            System.out.println(a);
+            a++;
+        }
+
+        System.out.println("New ");
+       int b = 10;
+        while (b <=50) {
+            System.out.println(b);
+            b++;
+
+    }
+
+
+
+}
+}
