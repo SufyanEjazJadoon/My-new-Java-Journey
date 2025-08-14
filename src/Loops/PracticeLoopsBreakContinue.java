2fb72893554526e0b6f36b5df1754bdc40d04355
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-14 22:58:01 +0500

    While,for,do while loop Practice

---
+++ src/Loops/PracticeLoopsBreakContinue.java
@@ -0,0 +1,21 @@
+package Loops;
+
+public class PracticeLoopsBreakContinue {
+    public static void main(String[] args) {
+
+        //Problem 1
+        int n = 4;
+        for (int i=n; i>0; i--){
+            for (int j=0; j<i; j++){
+                System.out.print("*");
+        }
+        System.out.print("\n");
+
+            //Problem 2
+
+
+
+
+    }
+    }
+}
