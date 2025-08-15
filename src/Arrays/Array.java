9e25a6da6e306ceff5e0d1abc5c187645f70953d
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-15 20:24:54 +0500

    Array.Sum of 5 Studend marks,Array in reverse order,Print array using loop

--- src/Arrays/Array.java
+++ src/Arrays/Array.java
@@ -16,6 +16,25 @@ public class Array {
         int[] marks1 = {12,33,44,55,66};
         System.out.println(marks1[4]);
 
+        //Check the length of array.
+        //System.out.println(marks1.length);
+
+        //Displaying of array.
+        //Applying for loop
+        System.out.println("Printing using for loop");
+        int[] marks2 = {12, 33, 44, 55, 66, 77};
+        for (int i = 0; i < marks2.length; i++){
+            System.out.println(marks2[i]);
+        }
+        //Quick Quiz
+        //array in reverse order
+        System.out.println("Array in reverse order");
+        for (int i=marks2.length -1; i >=0; i--) {
+            System.out.println(marks2[i]);
+        }
+
+
+
 
 
 
