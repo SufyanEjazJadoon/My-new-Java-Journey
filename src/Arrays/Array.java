1a3883765e996a7d3c9e1093707f7251b6a3f832
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-15 19:49:07 +0500

    sum of 5 students using Array.

---
+++ src/Arrays/Array.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        //Array!
+        //Sum of 5 students using array
+        int [] marks = new int[5];
+        marks[0] = 90;
+        marks[1] = 80;
+        marks[2] = 70;
+        marks[3] = 60;
+        marks[4] = 51;
+        System.out.println(marks[2]);
+        // there are two three types of array
+        //third one is
+        int[] marks1 = {12,33,44,55,66};
+        System.out.println(marks1[4]);
+
+
+
+
+
+    }
+}
