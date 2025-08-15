6820d480cd9717cbfeff324b295d59a870899919
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-15 20:51:23 +0500

    Displaying array using for each loop

--- src/Arrays/Array.java
+++ src/Arrays/Array.java
@@ -4,13 +4,13 @@ public class Array {
     public static void main(String[] args) {
         //Array!
         //Sum of 5 students using array
-        int [] marks = new int[5];
-        marks[0] = 90;
-        marks[1] = 80;
-        marks[2] = 70;
-        marks[3] = 60;
-        marks[4] = 51;
-        System.out.println(marks[2]);
+        int [] markss = new int[5];
+        markss[0] = 90;
+        markss[1] = 80;
+        markss[2] = 70;
+        markss[3] = 60;
+        markss[4] = 51;
+        System.out.println(markss[2]);
         // there are two three types of array
         //third one is
         int[] marks1 = {12,33,44,55,66};
@@ -38,12 +38,5 @@ public class Array {
         for (int element: marks2){
             System.out.println(element);
         }
-
-
-
-
-
-
-
     }
 }
