ca9e603d633243731acf6eb440a932f4088a608a
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-15 22:33:30 +0500

    Practice Array. cheak Value is present in array or not

--- src/Arrays/PractiseOfArray.java
+++ src/Arrays/PractiseOfArray.java
@@ -9,6 +9,22 @@ float [] marks = { 12.2f, 13.2f, 14.3f, 15.4f, 16.5f,17.6f};
 sum = sum + element;
             }
         System.out.println("The value of sum is " + sum);
+
+            //Problem 2
+        //
+        float [] markss = { 12.2f, 13.2f, 14.3f, 15.4f, 16.5f,17.6f};
+        float num = 13.2f;
+        boolean isInArray = false;
+        for (float elements:markss){
+            if (num ==elements){
+isInArray = true;
+            break;}
+        }
+        if (isInArray)
+            System.out.println("This value is present in the array");
+    else {
+            System.out.println("This value is not present in th array");
+        }
     }
 
 }
