f45d58ca51c928f029ee5d9d97643ee1f4cec2be
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-15 22:17:00 +0500

    Practise Array.Sum of 5 floats

---
+++ src/Arrays/PractiseOfArray.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class PractiseOfArray {
+    public static void main(String[] args) {
+        //Problem 1
+float [] marks = { 12.2f, 13.2f, 14.3f, 15.4f, 16.5f,17.6f};
+            float sum = 0;
+            for (float element:marks){
+sum = sum + element;
+            }
+        System.out.println("The value of sum is " + sum);
+    }
+
+}
