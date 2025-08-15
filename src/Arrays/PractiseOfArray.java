bef36f6cbe6ef90a5abb314a82cd9e9bd8680d73
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-15 23:39:40 +0500

    Practice Array. Make 2x3 matrix,

--- src/Arrays/PractiseOfArray.java
+++ src/Arrays/PractiseOfArray.java
@@ -3,37 +3,62 @@ package Arrays;
 public class PractiseOfArray {
     public static void main(String[] args) {
         //Problem 1
-float [] marks = { 12.2f, 13.2f, 14.3f, 15.4f, 16.5f,17.6f};
-            float sum = 0;
-            for (float element:marks){
-sum = sum + element;
-            }
+        float[] marks = {12.2f, 13.2f, 14.3f, 15.4f, 16.5f, 17.6f};
+        float sum = 0;
+        for (float element : marks) {
+            sum = sum + element;
+        }
         System.out.println("The value of sum is " + sum);
 
-            //Problem 2
+        //Problem 2
         //
-        float [] markss = { 12.2f, 13.2f, 14.3f, 15.4f, 16.5f,17.6f};
+        float[] markss = {12.2f, 13.2f, 14.3f, 15.4f, 16.5f, 17.6f};
         float num = 13.2f;
         boolean isInArray = false;
-        for (float elements:markss){
-            if (num ==elements){
-isInArray = true;
-            break;}
+        for (float elements : markss) {
+            if (num == elements) {
+                isInArray = true;
+                break;
+            }
         }
         if (isInArray)
             System.out.println("This value is present in the array");
-    else {
+        else {
             System.out.println("This value is not present in th array");
         }
 
-    //Problem 3
-        float [] marks1 = { 45.7f, 67.8f, 63.4f, 99.2f, 100.0f};
+        //Problem 3
+        float[] marks1 = {45.7f, 67.8f, 63.4f, 99.2f, 100.0f};
         float sum1 = 0;
-        for (float elementss:marks1){
+        for (float elementss : marks1) {
             sum1 = sum1 + elementss;
         }
-        System.out.println("The Value of average marks is" + sum1/marks1.length);
+        System.out.println("The Value of average marks is" + sum1 / marks1.length);
+        System.out.println(" ");
 
+        //problem 4
+        //2X3 matrix 2D
+        System.out.println(" ");
+        int[][] mat1 = {{1, 2, 3},
+                {4, 5, 6}};
+        int[][] mat2 = {{6, 5, 4},
+                {5, 3, 7}};
+        int[][] result = {{0, 0, 0},
+                {0, 0, 0}};
+        for (int i = 0; i < mat1.length; i++) {
+            for (int j = 0; j < mat1[i].length; j++) {
+                System.out.format("setting value for i=%d and j=%d\n", i, j);
+                result[i][j] = mat1[i][j] + mat2[i][j];
+                System.out.println(" ");
+            }
+        }
+        for (int i = 0; i < mat1.length; i++) {
+            for (int j = 0; j < mat1[i].length; j++) {
+                System.out.print(result[i][j] + "");
+                result[i][j] = mat1[i][j] + mat2[i][j];
+            }
+            System.out.println(" ");
+        }
     }
 
 }
