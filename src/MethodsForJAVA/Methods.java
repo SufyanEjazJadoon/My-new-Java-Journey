9d0e8b0cd4a3b59cc16372e9646612eaa3524d13
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-16 23:58:37 +0500

    Methods Java

---
+++ src/MethodsForJAVA/Methods.java
@@ -0,0 +1,24 @@
+package MethodsForJAVA;
+
+public class Methods {
+    public static void main(String[] args) {
+        //Methods of java.
+        int a = 10;
+        int b = 20;
+        int c;
+        if (a > b) {
+            c = a + b;
+        } else
+            c = (a + b) * 5;
+        System.out.println(" ");
+        int a1 = 30;
+        int b1 = 20;
+        int c1;
+        if (a1>b1){
+            c1 = a1 + b1;
+        }else
+            c1 = (a1 + b1)*5;
+        System.out.println(c);
+        System.out.println(c1);
+    }
+}
