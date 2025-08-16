d7435e649f6f974a97467bdf7d3c02366a525869
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-16 22:01:36 +0500

    Revert "Update Elseif.java"
    This reverts commit 4a0595e49e39da5c789c7fcabd75861ebc3bed8e.

--- src/Conditions/Elseif.java
+++ src/Conditions/Elseif.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Elseif {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
- 
+
         // Age check
         System.out.println("Enter your age");
         int age = sc.nextInt();
