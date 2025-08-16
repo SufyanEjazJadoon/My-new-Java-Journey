10cb96f6b9db2f68d0da10e7695f24b78da1e8f5
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-17 00:39:27 +0500

    Methods Java, Calling Method

--- src/MethodsForJAVA/Methods.java
+++ src/MethodsForJAVA/Methods.java
@@ -1,24 +1,41 @@
 package MethodsForJAVA;
 
 public class Methods {
+    /*Methods" of java.
+    Method ek chhota function hota hai jo code ka ek specific kaam karta hai.
+    Static method.
+     we use this method to not write a function again and again.*/
+    static int logic(int x, int y){
+        int z;
+        if (x > y) {
+            z = x + y;
+        } else {
+            z = (x + y) * 5;
+        }
+return z;
+    }
     public static void main(String[] args) {
-        //Methods of java.
-        int a = 10;
-        int b = 20;
-        int c;
-        if (a > b) {
-            c = a + b;
-        } else
-            c = (a + b) * 5;
+
+        int a = 20;
+        int b = 30;
+        int c = logic(a, b);
+
+
         System.out.println(" ");
         int a1 = 30;
         int b1 = 20;
-        int c1;
-        if (a1>b1){
-            c1 = a1 + b1;
-        }else
-            c1 = (a1 + b1)*5;
+        int c1 = logic(a1, b1);
         System.out.println(c);
         System.out.println(c1);
-    }
-}
+
+/*Calling method.
+is ka mtlb hota hai k ham method ko kis name say bulayengay ore new code k saath use kray-
+ gay jesay add, logic etc
+ */
+
+
+
+
+
+   }
+           }
