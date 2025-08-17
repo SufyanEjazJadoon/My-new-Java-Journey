7fe5c6fe5e0e0688f67b4c5d816c3cc413a7925c
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-18 01:03:14 +0500

    Practise recursion Star Triangle
    Recursion stars Triangle Right

--- src/Recursion/J/PracticeRecursion.java
+++ src/Recursion/J/PracticeRecursion.java
@@ -1,7 +1,7 @@
 package Recursion.J;
 
 public class PracticeRecursion {
-    static int fibonachi(int n){
+    static int fibonachi(int n) {
         if (n <= 1)
             return n;
         return fibonachi(n - 1) + fibonachi(n - 2);
@@ -10,8 +10,18 @@ public class PracticeRecursion {
     public static void main(String[] args) {
         int n = 10;
         System.out.println("Fibonacci series");
-        for (int i = 0; i < n; i++){
-            System.out.print(fibonachi(i)+ " ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonachi(i) + " ");
+        }
+        System.out.println(" ");
+        
+        //Star Triangle
+        int a = 4;
+        for (int b = 1; b <= a; b++) {
+            for (int c = 1; c <= b; c++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
     }
 }
