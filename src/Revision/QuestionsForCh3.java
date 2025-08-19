6720580243bf97fc993d69abfc86a44956dc1733
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-19 22:30:14 +0500

    String to Lowercase.
    Lowercase may convert kiya hai.

---
+++ src/Revision/QuestionsForCh3.java
@@ -0,0 +1,11 @@
+package Revision;
+
+public class QuestionsForCh3 {
+    public static void main(String[] args) {
+        String school = "Shaheen Public School";
+        //Write a java program to convert string to Lowercase
+        System.out.println(school.toLowerCase());
+
+        
+    }
+}
