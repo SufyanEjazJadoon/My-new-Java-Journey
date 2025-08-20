983851574e301003686af4737416a85e8128d3ae
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-20 22:14:56 +0500

    While Loop

---
+++ src/Revision/LoopsCh5.java
@@ -0,0 +1,22 @@
+package Revision;
+
+public class LoopsCh5 {
+    public static void main(String[] args) {
+        //Loops
+        //Three types of loops
+        //While loop
+        System.out.println("Ascending order");
+        int a = 1;
+        while (a <= 10){
+            System.out.println(a);
+            a++;
+        }
+        System.out.println(" ");
+        System.out.println("Descending order");
+        int b = 20;
+        while (b>=1){
+            System.out.println(b);
+            b--;
+        }
+    }
+}
