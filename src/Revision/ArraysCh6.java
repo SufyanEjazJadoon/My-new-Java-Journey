2c27dad439c24ccc7004f5ebd8d50144ae6fb767
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-20 23:59:07 +0500

    Three types of array.

---
+++ src/Revision/ArraysCh6.java
@@ -0,0 +1,30 @@
+package Revision;
+
+public class ArraysCh6 {
+    public static void main(String[] args) {
+        // Arrays
+        System.out.println("Array Types.");
+        //1
+        int[] marks = new int[3];
+        marks[0] = 10;
+        marks[1] = 20;
+        marks[2] = 30;
+        System.out.println(marks[2]);
+        System.out.println( );
+
+        //2
+int [] mark = {11,12,13,14,15,16};
+        System.out.println(mark[2]);
+        System.out.println();
+
+        //3
+int [] markss;
+markss = new int[5];
+        markss[0] = 11;
+        markss[1] = 21;
+        markss[2] = 31;
+        markss[3] = 41;
+        markss[4] = 51;
+        System.out.println(markss[4]);
+    }
+}
