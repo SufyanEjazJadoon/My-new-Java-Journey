4f1033ed79cf9420c97fe18f483f03b084e75704
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-20 23:00:53 +0500

    Print stars using for loop

---
+++ src/Revision/QuestionsForCh5.java
@@ -0,0 +1,14 @@
+package Revision;
+
+public class QuestionsForCh5 {
+    public static void main(String[] args) {
+
+        //Print stars
+        for (int a =5; a>1;a--){
+            for (int j = 1; j<= a; j++){
+                System.out.print("* ");
+            }
+            System.out.println( );
+        }
+    }
+}
