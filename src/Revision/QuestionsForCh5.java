1aade42e80fe5b35d817fa2bce75deee7aca3f6d
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-20 23:17:19 +0500

    Sum of first five Even numbers,

--- src/Revision/QuestionsForCh5.java
+++ src/Revision/QuestionsForCh5.java
@@ -4,11 +4,21 @@ public class QuestionsForCh5 {
     public static void main(String[] args) {
 
         //Print stars
-        for (int a =5; a>1;a--){
-            for (int j = 1; j<= a; j++){
+        for (int a = 5; a > 1; a--) {
+            for (int j = 1; j <= a; j++) {
                 System.out.print("* ");
             }
-            System.out.println( );
+            System.out.println();
         }
+        // sum of first five even numbers
+        int sum = 0;
+        int i = 5;
+        int n = 0;
+        while (i >= n) {
+            sum = sum + (2 * n);
+            n++;
+        }
+        System.out.print("Sum of first 5 even numbers: ");
+        System.out.println(sum);
     }
 }
