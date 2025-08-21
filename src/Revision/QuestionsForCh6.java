05b902e2e5fe9de61b37af9daa06df90555587da
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-21 22:58:42 +0500

    2X3 Matrix

--- src/Revision/QuestionsForCh6.java
+++ src/Revision/QuestionsForCh6.java
@@ -34,5 +34,22 @@ public class QuestionsForCh6 {
         } else {
             System.out.println(num + " is NOT in the array");
         }
+   // 2x3 matrix
+        System.out.println(" ");
+        int[][] mat1 = {{1, 2, 3},
+                {4, 5, 6}};
+        int[][] mat2 = {{6, 5, 4},
+                {5, 3, 7}};
+        int[][] result = {{0, 0, 0},
+                {0, 0, 0}};
+        for (int i = 0; i < mat1.length; i++) {
+            for (int j = 0; j < mat1[i].length; j++) {
+                System.out.format("setting value for i=%d and j=%d\n", i, j);
+                result[i][j] = mat1[i][j] + mat2[i][j];
+                System.out.println(" ");
+            }
+        }
+
+
     }
 }
