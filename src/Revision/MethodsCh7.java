56fd29f43da684934b2127efc0feea2e964b85f6
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-22 21:48:30 +0500

    Complete Method Overloading

--- src/Revision/MethodsCh7.java
+++ src/Revision/MethodsCh7.java
@@ -10,7 +10,27 @@ public class MethodsCh7 {
         }
         return z;
     }
+    //Method Overloading
+    static void change(int [] arr){
+        arr [0] = 25;
+    }
 
+    static void joke (){
+        System.out.println("Manay aik online store say book mangwayi thi how to scam Online,\n" +
+                " ab 2 mahinay guzr gay hain book nhi ayi");
+    }
+    //method overloading foo
+    static void foo(){
+        System.out.println("I Am Sufyan");
+    }
+    static void foo(int a) {
+        System.out.println("I Am Sufyan" + a);
+    }
+
+    static void foo(int a, int b){
+        System.out.println("Harry son " + a + " years ");
+        System.out.println("Harry sister " + b + " years ");
+    }
     public static void main(String[] args) {
         //Methods
         int a = 10;
@@ -23,5 +43,18 @@ public class MethodsCh7 {
     int c1 = logic(a1,b1);
         System.out.println(c1);
 
+        //Method Overloading
+        System.out.println("Method Overloading");
+        int [] marks = new int[4] ;
+        marks [0] = 10;
+        marks [1] = 10;
+        marks [2] = 10;
+        marks [3] = 10;
+        change(marks);
+        System.out.println("Marks changed by Method Overloading " + marks[0]);
+        foo();
+        foo(12);
+        foo(11,22);
+      joke();
     }
 }
