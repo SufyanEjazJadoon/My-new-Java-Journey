623bd45a440c2c85ed4d9754abb3259845b8d519
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-08-22 21:15:55 +0500

    Methods

--- src/Revision/MethodsCh7.java
+++ src/Revision/MethodsCh7.java
@@ -1,8 +1,27 @@
 package Revision;
 
 public class MethodsCh7 {
+    static int logic(int x, int y) {
+        int z;
+        if (x > y) {
+            z = x + y;
+        } else {
+            z = (x + y) * 2;
+        }
+        return z;
+    }
+
     public static void main(String[] args) {
         //Methods
-        
+        int a = 10;
+        int b = 20;
+        int c = logic(a, b);
+        System.out.println(c);
+        System.out.println();
+    int a1 = 20;
+    int b1 = 10;
+    int c1 = logic(a1,b1);
+        System.out.println(c1);
+
     }
 }
