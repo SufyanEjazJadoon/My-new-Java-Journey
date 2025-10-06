8996f8aa7181134fdbb1226b68a603d3fd8aaf5e
SufyanEjazJadoon <sufyanjadoon134@gmail.com>
2025-10-06 22:36:30 +0500

    Topic Access modifier

---
+++ src/AccessModifier/AccessModifier.java
@@ -0,0 +1,12 @@
+package AccessModifier;
+class MyEmployee{
+    int Id;
+    String Name;
+}
+
+public class AccessModifier {
+    public static void main(String[] args) {
+        MyEmployee Sufyan = new MyEmployee();
+
+    }
+}
